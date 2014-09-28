aece4d63723625a82286af1661fe08e2eca2bdba
Marko Rakić <markorakic@Markos-MacBook-Pro.local>
2014-09-28 21:37:31 +0200

    getText() function added
    Returns text as:
    
    id, author, title, body

--- services/api.php
+++ services/api.php
@@ -1,148 +1,184 @@
 <?php
 
-  /***************************/
-  /* DASUV           api.php */
-  /***************************/
+	/***************************/
+	/* DASUV           api.php */
+	/***************************/
 
-  /* Check for method */
-  if(isset($_GET['method']) && !empty($_GET['method'])) {
-       
-    if(function_exists($_GET['method'])) {
-        $_GET['method']();
-      }
-    } else {
-      echo 'Method needs to be specified';
-    }
+	/* Check for method */
+	if(isset($_GET['method']) && !empty($_GET['method'])) {
+			 
+		if(function_exists($_GET['method'])) {
+				$_GET['method']();
+			}
+		} else {
+			echo 'Method needs to be specified';
+		}
 
-  /* Get categories from db */
-  function getCategories(){
-      require('init.php'); 
-      $query = $db -> prepare("SELECT * FROM categories");
-      $query -> execute(); $rezultat = $query -> fetchAll();
+	/* Get categories from db */
+	function getCategories(){
+			require('init.php'); 
+			$query = $db -> prepare("SELECT * FROM categories");
+			$query -> execute(); $rezultat = $query -> fetchAll();
 
-      foreach($rezultat as $r){
-        $categories[] = array(
-          'id' => intval($r['id']),
-          'label' => $r['cat_title_'.$_GET['lang']]
-        );
-      }
-    /* -> return */
-    echo json_encode($categories);
-  }
+			foreach($rezultat as $r){
+				$categories[] = array(
+					'id' => intval($r['id']),
+					'label' => $r['cat_title_'.$_GET['lang']]
+				);
+			}
+		/* -> return */
+		echo json_encode($categories);
+	}
 
-  /* Get artist from db */
-  function getArtist(){
-    
-    $artist_id = $_GET['artist_id'];
+	/* Get artist from db */
+	function getArtist(){
+		
+		$artist_id = $_GET['artist_id'];
 
-    if(isset($artist_id) && !empty($artist_id)) {
+		if(isset($artist_id) && !empty($artist_id)) {
 
-      $lang = $_GET['lang'];
+			$lang = $_GET['lang'];
 
-      /* artist info */
-      require('init.php'); 
-      $query = $db -> prepare("SELECT * FROM artists INNER JOIN categories ON artists.category_id=categories.id AND artists.artist_id=".$artist_id);
-      $query -> execute(); $rezultat = $query -> fetchAll();
+			/* artist info */
+			require('init.php'); 
+			$query = $db -> prepare("SELECT * FROM artists INNER JOIN categories ON artists.category_id=categories.id AND artists.artist_id=".$artist_id);
+			$query -> execute(); $rezultat = $query -> fetchAll();
 
-      foreach($rezultat as $r){
-        $artist_full['artist'] = array(
-          'id' => intval($r['artist_id']),
-          'category_id' => intval($r['category_id']),
-          'category_name' => $r['cat_title_'.$lang],
-          'name' => $r['name_'.$lang],
-          'website' => $r['website'],
-          'biography' => $r['bio_'.$lang],
-          'path' => 'avatars/'.$r['artist_id'].'.jpg'
-        );
-      }
+			foreach($rezultat as $r){
+				$artist_full['artist'] = array(
+					'id' => intval($r['artist_id']),
+					'category_id' => intval($r['category_id']),
+					'category_name' => $r['cat_title_'.$lang],
+					'name' => $r['name_'.$lang],
+					'website' => $r['website'],
+					'biography' => $r['bio_'.$lang],
+					'path' => 'avatars/'.$r['artist_id'].'.jpg'
+				);
+			}
 
-      /* get artworks for artist */
-      $query = $db -> prepare("SELECT * FROM artworks WHERE artist_id=".$artist_id);
-      $query -> execute(); $rezultat = $query -> fetchAll();
+			/* get artworks for artist */
+			$query = $db -> prepare("SELECT * FROM artworks WHERE artist_id=".$artist_id);
+			$query -> execute(); $rezultat = $query -> fetchAll();
 
-      foreach($rezultat as $r){
-        $artworks[] = array(
-          'id' => intval($r['id']),
-          'title' => $r['title_'.$lang],
-          'year' => intval($r['year']),
-          'media_type' => $r['media_type'],
-          'path' => 'media/'.$r['id'].'/thumb.jpg'
-        );
-      }
-      $artist_full['artworks'] = $artworks;
+			foreach($rezultat as $r){
+				$artworks[] = array(
+					'id' => intval($r['id']),
+					'title' => $r['title_'.$lang],
+					'year' => intval($r['year']),
+					'media_type' => $r['media_type'],
+					'path' => 'media/'.$r['id'].'/thumb.jpg'
+				);
+			}
+			$artist_full['artworks'] = $artworks;
 
-    }
-    else{
-      echo 'No category id specified.';
-    }
-    /* -> return */
-    echo json_encode($artist_full);
-  }
+		}
+		else{
+			echo 'No category id specified.';
+		}
+		/* -> return */
+		echo json_encode($artist_full);
+	}
 
 
-  function getSearchResults(){
-    $results = array(
-      array(
-        'id'        => 1,
-        'title' => 'Some text ',
-        'author'  => 'Some Author',
-      ),
-      array(
-        'id'        => 2,
-        'title' => 'Some other text',
-        'author'  => 'Some Other',
-      ),
-    );
-    
-    /* -> return */
-    echo json_encode($results);
-  }
+	function getSearchResults(){
+		$results = array(
+			array(
+				'id'        => 1,
+				'title' => 'Some text ',
+				'author'  => 'Some Author',
+			),
+			array(
+				'id'        => 2,
+				'title' => 'Some other text',
+				'author'  => 'Some Other',
+			),
+		);
+		
+		/* -> return */
+		echo json_encode($results);
+	}
 
-  /* Get artist, artworks & texts from category id */  
-  function getCategory(){
+	/* Get artist, artworks & texts from category id */  
+	function getCategory(){
 
-    $category_id = $_GET['category_id'];
-    
-    if(isset($category_id) && !empty($category_id)) {
+		$category_id = $_GET['category_id'];
+		
+		if(isset($category_id) && !empty($category_id)){
 
-      require('init.php');
+				require('init.php');
 
-        /* get category title */
-        $query = $db -> prepare("SELECT * FROM categories WHERE id=".$category_id);
-        $query -> execute(); $rezultat = $query -> fetchAll();
-        $categories['category_name'] = $rezultat[0]['cat_title_'.$_GET['lang']];
+				/* get category title */
+				$query = $db -> prepare("SELECT * FROM categories WHERE id=".$category_id);
+				$query -> execute(); $rezultat = $query -> fetchAll();
+				$categories['category_name'] = $rezultat[0]['cat_title_'.$_GET['lang']];
 
-        /* get artists */
-        $query = $db -> prepare("SELECT * FROM artists WHERE category_id=".$category_id." LIMIT 4");
-        $query -> execute(); $rezultat = $query -> fetchAll();
-        foreach($rezultat as $r){
-          $umetnici[] = array(
-            'id' => intval($r['artist_id']),
-            'name' => $r['name_'.$_GET['lang']],
-            'path' => 'avatars/'.$r['artist_id'].'.jpg'
-          );
-        }
-        $categories['artists'] = $umetnici;
+				/* get artists */
+				$query = $db -> prepare("SELECT * FROM artists WHERE category_id=".$category_id." LIMIT 4");
+				$query -> execute(); $rezultat = $query -> fetchAll();
+				foreach($rezultat as $r){
+					$umetnici[] = array(
+						'id' => intval($r['artist_id']),
+						'name' => $r['name_'.$_GET['lang']],
+						'path' => 'avatars/'.$r['artist_id'].'.jpg'
+					);
+				}
+				$categories['artists'] = $umetnici;
 
-        /* get artworks */
-        $query = $db -> prepare("SELECT * FROM artworks WHERE category_id=".$category_id." LIMIT 4");
-        $query -> execute(); $rezultat = $query -> fetchAll();
-        foreach($rezultat as $r){
-          $dela[] = array(
-            'id' => intval($r['id']),
-            'title' => $r['title_'.$_GET['lang']],  
-            'path' => 'media/'.$r['id'].'/thumb.jpg'
-          );
-        }
-        $categories['artworks'] = $dela;
+				/* get artworks */
+				$query = $db -> prepare("SELECT * FROM artworks WHERE category_id=".$category_id." LIMIT 4");
+				$query -> execute(); $rezultat = $query -> fetchAll();
+				foreach($rezultat as $r){
+					$dela[] = array(
+						'id' => intval($r['id']),
+						'title' => $r['title_'.$_GET['lang']],  
+						'path' => 'media/'.$r['id'].'/thumb.jpg'
+					);
+				}
+				$categories['artworks'] = $dela;
 
-    /* -> return */
-    echo json_encode($categories);
+			/* -> return */
+			echo json_encode($categories);
+	
+		}else{
+			echo 'No category_id is specified.';
+	}
 
-    
-    }else{
-      echo 'No category id specified.';
-    }
-  }
 
+	/* Get single text from text_id */
+	function getText(){
+	
+		$text_id = $_GET['text_id'];
+		$lang = $_GET['lang'];
+
+		if(isset($text_id) && !empty($text_id)) {
+ 		
+			$query = $db -> prepare("SELECT * FROM texts WHERE text_id=".$text_id);
+			$query -> execute(); $rezultat = $query -> fetchAll();
+
+			foreach($rezultat as $r){
+				$text[] = array(
+					'id' => intval($r['text_id']),
+					'author' => $r['author_'.$lang],
+					'title' => $r['title_'.$lang],  
+					'body' => $r['body_'.$lang]
+				);
+			}
+			/* -> return */
+			echo json_encode($text);
+
+ 			}else{
+ 				echo 'No text_id specified.';
+ 			}
+ 	}
+
+ 	/* MORE FUNCTIONS! */
+
+
+
+
+
+	
+
+	}
+	/* end. */
 ?>
