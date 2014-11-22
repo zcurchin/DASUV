bf84bcb7c2df3617ed64a8a467d9dc38e805aecf
Marko Rakić <marko@Markos-MacBook-Air.local>
2014-11-22 19:08:01 +0100

    Izmene

--- services/init.php
+++ services/init.php
@@ -7,30 +7,29 @@
 	error_reporting(0);
 
 	/* Konfiguracija, ukoliko je baza u lokalu */
-	/*
+
 	$config['db'] = array(
 		'host' => 'localhost',
 		'user' => 'root',
 		'pass' => 'root',
-		'dbname' => 'DASUV'
-	);
-	*/
-
-	
-	$config['db'] = array(
-		'host' => 'dasuv.db.10233176.hostedresource.com',
-		'user' => 'dasuv',
-		'pass' => 'Zavod666!',
 		'dbname' => 'dasuv'
 	);
-	
+
+
+	// $config['db'] = array(
+	// 	'host' => 'dasuv.db.10233176.hostedresource.com',
+	// 	'user' => 'dasuv',
+	// 	'pass' => 'Zavod666!',
+	// 	'dbname' => 'dasuv'
+	// );
+	//
 
 	$db = new PDO(
 		'mysql:host='.$config['db']['host'].
 		';dbname='.$config['db']['dbname'],
 		$config['db']['user'],
 		$config['db']['pass']
-	);	
+	);
 
 	$db -> exec("SET CHARACTER SET utf8");
 
